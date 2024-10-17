a70e97e1fbadaea7e99822056c617e43e5ea987e
Jean-Michel Cid <77751904+cjeanm11@users.noreply.github.com>
2024-10-17 19:35:48 -0400

    Create create_min_pdf.sh

---
+++ create_min_pdf.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Create a minimal PDF structure
+echo "%PDF-1.4
+1 0 obj
+<< /Type /Catalog /Pages 2 0 R >>
+endobj
+2 0 obj
+<< /Type /Pages /Kids [3 0 R] /Count 1 >>
+endobj
+3 0 obj
+<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R >>
+endobj
+4 0 obj
+<< /Length 44 >>
+stream
+BT
+/F1 24 Tf
+100 700 Td
+(Hello, World!) Tj
+ET
+endstream
+endobj
+5 0 obj
+<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>
+endobj
+xref
+0 6
+0000000000 65535 f 
+0000000010 00000 n 
+0000000060 00000 n 
+0000000116 00000 n 
+0000000177 00000 n 
+0000000266 00000 n 
+trailer
+<< /Size 6 /Root 1 0 R >>
+startxref
+341
+%%EOF" > mydocument.pdf
